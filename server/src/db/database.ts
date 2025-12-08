da11bd10d83f235b345b1af7bd2fab3f39a84966
VP <alpha@VPs-MacBook-Air.local>
2025-12-09 00:45:06 +0530

    feat(core): add structured logging, database connection utility, and centralized config loader

---
+++ server/src/db/database.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import config from "../config/config";
+import logger from '../utils/logger';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(config.MONGODB_URL!);
+    logger.info("Database is connected");
+  } catch (error) {
+    logger.warn("Error connecting to database");
+    process.exit(1);
+  }
+};
+
+export default connectDB
---
+++ server/src/utils/logger.ts
@@ -0,0 +1,21 @@
+import winston from "winston";
+
+const consoleFormat = winston.format.combine(
+  winston.format.colorize(),
+  winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+  winston.format.printf(({ timestamp, level, message, ...meta }) => {
+    let msg = `${timestamp} [${level}]: ${message}`;
+    if (Object.keys(meta).length > 0) {
+      msg += `\n${JSON.stringify(meta, null, 2)}`;
+    }
+    return msg;
+  })
+);
+
+const logger = winston.createLogger({
+  level: "debug",
+  format: consoleFormat,
+  transports: [new winston.transports.Console()],
+});
+
+export default logger;
