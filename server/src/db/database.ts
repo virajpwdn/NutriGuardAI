d931a2cf933283011a371c8c0d3bdcfc0aa82eb5
VP <alpha@VPs-MacBook-Air.local>
2025-12-09 01:04:44 +0530

    fix: type asseration

--- server/src/db/database.ts
+++ server/src/db/database.ts
@@ -4,7 +4,7 @@ import logger from '../utils/logger';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(config.MONGODB_URL!);
+    await mongoose.connect(config.MONGODB_URL);
     logger.info("Database is connected");
   } catch (error) {
     logger.error("Error connecting to database");
