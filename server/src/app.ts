b619a65701499fc8aa0528ea1982caba8bc52035
VP <alpha@VPs-MacBook-Air.local>
2025-12-09 20:37:19 +0530

    feat: test agent created

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import cookieparser from "cookie-parser";
+import aiRouter from "./routes/auth.routes";
 
 const app = express();
 
@@ -11,9 +12,6 @@ app.use(cookieparser());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.get("/test", (req, res) => {
-  console.log("HELLO WORLD");
-  res.send("Hello");
-});
+app.use("/v1/ai", aiRouter)
 
 export default app;
